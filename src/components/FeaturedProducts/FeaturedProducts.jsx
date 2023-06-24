c772be33c1b9b849c88c066d0cd0e9290863bd2b
unknown <dj125466@gmail.com>
2023-06-24 12:56:16 +0200

    updated Homepage

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
-import './FeaturedProducts.scss'
+import './FeaturedProducts.scss';
+import Cards from '../Cards/Cards';
 
-const FeaturedProducts = () => {
+const FeaturedProducts = ({type}) => {
+
+    const data = [
+        {
+            id: 1,
+            img: './images/arianna2.jpg',
+            img2: './images/arianna1.jpg',
+            title: 'Long Sleeve Graphic T-shirt',
+            isNew: true,
+            oldPrice: 19,
+            price: 12,
+        },
+        {
+            id: 2,
+            img: './images/arianna2.jpg',
+            title: 'Long Sleeve Graphic T-shirt',
+            isNew: true,
+            oldPrice: 19,
+            price: 12,
+        },
+        {
+            id: 3,
+            img: './images/arianna2.jpg',
+            title: 'Long Sleeve Graphic T-shirt',
+            oldPrice: 19,
+            price: 12,
+        },
+        {
+            id: 4,
+            img: './images/arianna2.jpg',
+            title: 'Long Sleeve Graphic T-shirt',
+            oldPrice: 19,
+            price: 12,
+        },
+    ]
+
   return (
-    <div className='featuredProducts'>FeaturedProducts</div>
+    <div className='featuredProducts'>
+        <div className="top">
+            <h1>{type} products</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur veritatis laudantium 
+                tempore earum consequatur commodi dolorum ab dolor at eligendi vero cumque laboriosam 
+                eum tenetur et, nobis dolore soluta est?
+            </p>
+        </div>
+        <div className="bottom">
+            {data.map(item => (
+                <Cards item={item} key={item.id}/>
+                ))}
+        </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,19 @@
-import  Slider from '../../components/Slider/Slider';
 import React from 'react';
+import  Slider from '../../components/Slider/Slider';
 import './Home.scss';
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
+import Categories from '../../components/Categories/Categories';
+import Contacts from '../../components/Contacts/Contacts';
 
 const Home = () => {
+  
   return (
     <div className='home'>
       <Slider/>
-      <FeaturedProducts/>
+      <FeaturedProducts type='featured'/>
+      <Categories/>
+      <FeaturedProducts type='trending'/>
+      <Contacts/>
     </div>
   )
 }
